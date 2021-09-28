8e017143b0c05488b1a3e7f335e1eef7ae1f41df
Jefferson Rodrigues <jeffersonrodrigues5030@gmail.com>
2021-09-28 08:47:10 -0300

    criado cadastrar.php

---
+++ cadastrar.php
@@ -0,0 +1,22 @@
+<?php
+require('conexao.php');
+
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$instagram = $_POST['instagram'];
+$birth = $_POST['birth'];
+
+$sql = 'INSERT INTO usuarios (name, phone, instagram, birth) VALUES (?, ?, ?, ?);';
+
+$stm = $pdo->prepare($sql);
+
+$stm->bindValue(1, $name);
+$stm->bindValue(2, $phone);
+$stm->bindValue(3, $instagram);
+$stm->bindValue(4, $birth);
+
+$stm->execute();
+
+
+
+?>

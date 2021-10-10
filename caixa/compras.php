7955bee166f4a7408b1e4092b84eea86a91851a4
Jefferson Rodrigues <jeffersonrodrigues5030@gmail.com>
2021-10-10 01:59:03 -0300

    salvando compras ok

--- caixa/compras.php
+++ caixa/compras.php
@@ -4,44 +4,51 @@ require('../conexao.php');
 
 $user_id = $_POST['user_id'];
 $servicos = $_POST['servicos'];
-$compra = 150;#$_POST['compra'];
 
 $string = implode(",", $servicos); 
 
-echo "id: ".$user_id."<br>";
-echo "implodido ".$string."<br>";
-echo "compra: ".$compra."<br>";
+#echo "id: ".$user_id."<br>";
+#echo "implodido ".$string."<br>";
 
 $explode = explode(",", $string);
 
-echo "<br><br> explodido";
-print_r($explode);
+#echo "<br><br> explodido";
+#print_r($explode);
 
-foreach ($explode as $um) {
-    echo "<br><br>";
-    echo "item-> ".$um."<br><br>";
-}
+#foreach ($explode as $um) {
+#    echo "<br><br>";
+#    echo "item-> ".$um."<br><br>";
+#}
 
-print_r($explode);
+#print_r($explode);
 
 $soValores = [];
 
-/*$explode = array("laranja", "morango");
-array_push($cesta, "melancia", "batata");
-print_r($cesta);
-*/ 
-
 for ($i=0; $i < count($explode); $i++) { 
-    echo "<br>item ".$i." ---> ".$explode[$i]."<br>";
+#    echo "<br>item ".$i." ---> ".$explode[$i]."<br>";
 
     if ($i % 2 == 1) {
-        echo $i." é impar<br>";
+#        echo $i." é impar<br>";
         
         array_push($soValores, $explode[$i]);
     }
 }
 
-print_r($soValores);
+#print_r($soValores);
+
+function sum($carry, $item)
+{
+    $carry += $item;
+    return $carry;
+}
+
+$valorTotal = array_reduce($soValores, "sum");
+
+#echo "<br>***".$valorTotal."***<br>";
+
+#echo "<br>id: ".$user_id."<br>";
+#echo "<br>implodido ".$string."<br>";
+#echo "<br>compra: ".$valorTotal."<br>";
 
 
 $sql = 'INSERT INTO compras (user_id, servicos, compra) VALUES (?, ?, ?);';
@@ -49,8 +56,16 @@ $sql = 'INSERT INTO compras (user_id, servicos, compra) VALUES (?, ?, ?);';
 $stm = $pdo->prepare($sql);
 
 $stm->bindValue(1, $user_id);
-$stm->bindValue(2, $servicos);
-$stm->bindValue(3, $compra);
+$stm->bindValue(2, $string);
+$stm->bindValue(3, $valorTotal);
+
+$stm->execute();
+
+$sql = 'SELECT * FROM compras WHERE user_id = ?;';
+
+$stm = $pdo->prepare($sql);
+
+$stm->bindValue(1, $user_id);
 
 $stm->execute();
 

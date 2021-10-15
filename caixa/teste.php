1f4936270b0430c86f31fecfc1ed08b868bae324
Jefferson Rodrigues <jeffersonrodrigues5030@gmail.com>
2021-10-15 08:52:04 -0300

    lógica com a variavel

--- caixa/teste.php
+++ caixa/teste.php
@@ -12,28 +12,34 @@ LEFT JOIN table2
 ON table1.column_name = table2.column_name;
 */
 
-print_r($join);
-echo "<br><br>";
+#print_r($join);
+#echo "<br><br>";
 /*
 foreach ($join as $um) {
     echo "<br>".$um['name']." comprou ".$um['compra'].",00R$<br>";
 }
 */
+$arrayCompras = [];
 $arrayUsuarios = [];
-
+$name = "";
 
 for ($i=0; $i < count($join); $i++) { 
     
     $loops = 0;
     $somaCompras = 0;
+    
 
     for ($j=0; $j < count($join); $j++) { 
 
-        if ($join[$j]['name'] == $join[$i]['name']) {
-            echo "<br>".$join[$j]['name']." comprou ".$join[$j]['compra']."<br>";
+        if ($name != $join[$i]['name']) {
 
-            $somaCompras = $somaCompras + $join[$j]['compra'];
-            echo "<br>".$somaCompras."<br>";
+            if ($join[$j]['name'] == $join[$i]['name']) {
+                echo "<br>".$join[$j]['name']." comprou ".$join[$j]['compra']."<br>";
+    
+                $somaCompras = $somaCompras + $join[$j]['compra'];
+                echo "<br>".$somaCompras."<br>";
+            }
+
         }
 
         $loops++;
@@ -42,9 +48,40 @@ for ($i=0; $i < count($join); $i++) {
 
     echo "<br>Quantidade de  loops ".$loops."<br>";
     echo "<br> Terminada volta ".$i." com a compra no total de ".$somaCompras." para ".$join[$i]['name']." com id: ".$join[$i]['user_id']."<br>";
+    
+    $name = $join[$i]['name'];
+    echo "<br>".$name."<br>";
+    
+
+#    array_push($arrayCompras, [$join[$i]['name'] => $somaCompras]);
 }
+
+#print_r($arrayCompras);
+#print_r(rsort($arrayCompras));
+#print_r(usort($arrayCompras));
 
 
 
 ?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>teste</title>
+    <style>
+        body {
+            background-color: #333;
+            color: #ddd;
+        }
+    </style>
+</head>
+<body>
+    
+</body>
+</html>
 
